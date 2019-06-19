2f263f3056c6ebe08f5b50cd83308b6e4c51c501
tomokazu <tomokazu_1011@yahoo.co.jp>
2019-06-20 03:10:04 +0900

    190619home

--- htdocs/html/index.php
+++ htdocs/html/index.php
@@ -144,35 +144,175 @@
           <h2 class="contentTitle__main">最新の質問</h2>
           <div class="contentTitle__caption">新しく寄せられた質問にお答えいたします。</div>
         </div>
-        <ul class="qaList">
-          <li class="qaList__item">
-            <a class="qaList__itemInner">
-              <div class="qaList__itemEyecatch">
-                <img src="/assets/images/sample.jpg" class="qaList__itemEyecatchImg" alt="">
-              </div>
-              <div class="qaList__textBlock">
-                <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
-                <div class="qaList__itemClip">
-                  <div class="clipCounter">
-                    <div class="clipCounter__icon">
-                      <span class="Icon -facebook"></span>
+        <div class="qaListContainer">
+          <ul class="qaList">
+            <li class="qaList__item">
+              <a href="#" class="qaList__itemInner">
+                <div class="qaList__itemEyecatch" style="background-image: url('/assets/images/sample.jpg');">
+                  <div class="exceptSmall">
+                    <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                    <div class="qaList__itemClip">
+                      <div class="clipCounter">
+                        <div class="clipCounter__icon">
+                          <span class="Icon -facebook"></span>
+                        </div>
+                        <div class="clipCounter__num">255</div>
+                      </div>
                     </div>
-                    <div class="clipCounter__num">255</div>
+                  </div>
+                  <!-- <img src="/assets/images/sample.jpg" class="qaList__itemEyecatchImg" alt=""> -->
+                </div>
+                <div class="qaList__textBlock">
+                  <div class="onlySmall">
+                    <div class="qaList__status">
+                      <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                      <div class="qaList__itemClip">
+                        <div class="clipCounter">
+                          <div class="clipCounter__icon">
+                            <span class="Icon -facebook"></span>
+                          </div>
+                          <div class="clipCounter__num">255</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="qaList__date">2019.06.01</div>
+                  <h2 class="qaList__question">育児休暇はもらえますか？またその条件はありますか</h2>
+                  <div class="qaList__questionDescription">
+                    ミュゼプラチナムで正社員で勤務した場合勤続から何年目から育児休暇の取得が可能になりますか？<br>
+                    また、途中で社員から契約社員、あるいはパート勤務に変わった場合など、取得できる日数や、あるいは条件が変わることなどありますか？
                   </div>
                 </div>
-                
-                <div class="qaList__date">2019.06.01</div>
-                <h2 class="qaList__question">育児休暇はもらえますか？またその条件はありますか</h2>
-                <div class="qaList__questionDescription">
-                  ミュゼプラチナムで正社員で勤務した場合勤続から何年目から育児休暇の取得が可能になりますか？<br>
-                  また、途中で社員から契約社員、あるいはパート勤務に変わった場合など、取得できる日数や、あるいは条件が変わることなどありますか？
+              </a>
+            </li>
+            <li class="qaList__item">
+              <a href="#" class="qaList__itemInner">
+                <div class="qaList__itemEyecatch" style="background-image: url('/assets/images/sample.jpg');">
+                  <div class="exceptSmall">
+                    <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                    <div class="qaList__itemClip">
+                      <div class="clipCounter">
+                        <div class="clipCounter__icon">
+                          <span class="Icon -facebook"></span>
+                        </div>
+                        <div class="clipCounter__num">255</div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- <img src="/assets/images/sample.jpg" class="qaList__itemEyecatchImg" alt=""> -->
                 </div>
-              </div>
-            </a>
-          </li>
-        </ul>
+                <div class="qaList__textBlock">
+                  <div class="onlySmall">
+                    <div class="qaList__status">
+                      <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                      <div class="qaList__itemClip">
+                        <div class="clipCounter">
+                          <div class="clipCounter__icon">
+                            <span class="Icon -facebook"></span>
+                          </div>
+                          <div class="clipCounter__num">255</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="qaList__date">2019.06.01</div>
+                  <h2 class="qaList__question">育児休暇はもらえますか？またその条件はありますか</h2>
+                  <div class="qaList__questionDescription">
+                    ミュゼプラチナムで正社員で勤務した場合勤続から何年目から育児休暇の取得が可能になりますか？<br>
+                    また、途中で社員から契約社員、あるいはパート勤務に変わった場合など、取得できる日数や、あるいは条件が変わることなどありますか？
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="qaList__item">
+              <a href="#" class="qaList__itemInner">
+                <div class="qaList__itemEyecatch" style="background-image: url('/assets/images/sample.jpg');">
+                  <div class="exceptSmall">
+                    <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                    <div class="qaList__itemClip">
+                      <div class="clipCounter">
+                        <div class="clipCounter__icon">
+                          <span class="Icon -facebook"></span>
+                        </div>
+                        <div class="clipCounter__num">255</div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- <img src="/assets/images/sample.jpg" class="qaList__itemEyecatchImg" alt=""> -->
+                </div>
+                <div class="qaList__textBlock">
+                  <div class="onlySmall">
+                    <div class="qaList__status">
+                      <div class="qaList__itemCategory" style="background-color: #5360db;">休暇申請</div>
+                      <div class="qaList__itemClip">
+                        <div class="clipCounter">
+                          <div class="clipCounter__icon">
+                            <span class="Icon -facebook"></span>
+                          </div>
+                          <div class="clipCounter__num">255</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="qaList__date">2019.06.01</div>
+                  <h2 class="qaList__question">育児休暇はもらえますか？またその条件はありますか</h2>
+                  <div class="qaList__questionDescription">
+                    ミュゼプラチナムで正社員で勤務した場合勤続から何年目から育児休暇の取得が可能になりますか？<br>
+                    また、途中で社員から契約社員、あるいはパート勤務に変わった場合など、取得できる日数や、あるいは条件が変わることなどありますか？
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+          <div class="viewMore">
+            <a href="#" class="viewMore__button viewMore__button--large">質問の一覧をみる</a>
+          </div>
+        </div>
+      </section>
+
+      <section class="container__mainSection">
+        <a href="#" target="_blank"><img src="/assets/images/ad.jpg"></a>
+      </section>
+
+      <section class="container__mainSection">
+        <div class="contentTitle">
+          <h2 class="contentTitle__main">Q&Aクリップランキング</h2>
+          <div class="contentTitle__caption">求職者からクリップを集めている質問のランキングです</div>
+        </div>
+        <div class="ranking">
+          <div class="ranking__select">
+            カテゴリ
+            <select>
+              <option selected>全て</option>
+  						<option value="#">休暇申請</option>
+              <option value="#">就業</option>
+              <option value="#">面接・選考</option>
+            </select>
+          </div>
+          <ol class="ranking__list">
+            <li class="ranking__listItem">
+              <a href="#">育児休暇はいつから取得できますか<span class="ranking__listItemCategory">[休暇]</span></a>
+            </li>
+            <li class="ranking__listItem">
+              <a href="#">育児休暇はいつから取得できますか<span class="ranking__listItemCategory">[休暇]</span></a>
+            </li>
+            <li class="ranking__listItem">
+              <a href="#">育児休暇はいつから取得できますか<span class="ranking__listItemCategory">[休暇]</span></a>
+            </li>
+            <li class="ranking__listItem">
+              <a href="#">育児休暇はいつから取得できますか<span class="ranking__listItemCategory">[休暇]</span></a>
+            </li>
+            <li class="ranking__listItem">
+              <a href="#">育児休暇はいつから取得できますか<span class="ranking__listItemCategory">[休暇]</span></a>
+            </li>
+          </ol>
+        </div>
       </section>
     </div>
+
     <div class="container__side">
       sideContent
     </div>
